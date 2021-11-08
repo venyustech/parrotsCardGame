326959a674ee03f4f827e10af1a776e0aa152a09
Gardenia Georgia Barbosa De Siqueira <venyustech@gmail.com>
2021-11-08 05:49:18 -0300

    Adicionando regras para facilitar correção

--- script.js
+++ script.js
@@ -3,17 +3,21 @@ function loadPage(){
     
     if(cardsQuantity < 2 || cardsQuantity >7)
         do{
-            cardsQuantity= parseInt(prompt("Bença, tu digitou errado. Presta atenção, se liga:\nDigite quantidade de pares de cartas. (2 a 7)"));
+            cardsQuantity= parseInt(prompt("Bença, tu digitou errado. Se liga:\nDigite quantidade de pares de cartas. (2 a 7)"));
         }while((cardsQuantity < 2) || (cardsQuantity >7));
 
     const qtdCards = [];
     qtdCards[0]= "`";
-    console.log(qtdCards[0]);
+   
   
     for(i = 0; i < (2*cardsQuantity); i++)
     {
-        qtdCards[0] +=`<div class="card card${i+1}">
-    <img class="logoImage" src="assets/front.png" alt="card"/>
+        qtdCards[0] +=`<div class="card card${i+1}"data-identifier="card">
+    <div class="front" data-identifier="back-face">
+        <img class="logoImage" src="assets/front.png" alt="card"/>  
+    </div>
+    <div class="verse card${i+1} hide" identifier="front-face">            
+    </div>
 </div>
 ` 
     } 
