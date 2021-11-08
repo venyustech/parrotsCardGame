6b57a48d529797a45b036f8bbfe665a38c7089a4
Gardenia Georgia Barbosa De Siqueira <venyustech@gmail.com>
2021-11-08 05:15:27 -0300

    adicionando recursividade caso usuario digite errado

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 function loadPage(){
-    const cardsQuantity= parseInt(prompt("Qual a quantidade de pares de cartas? (1 a 7)"));
+    let cardsQuantity= parseInt(prompt("Qual a quantidade de pares de cartas? (2 a 7)"));
+    
+    if(cardsQuantity < 2 || cardsQuantity >7)
+        do{
+            cardsQuantity= parseInt(prompt("Bença, tu digitou errado. Presta atenção, se liga:\nDigite quantidade de pares de cartas. (2 a 7)"));
+        }while((cardsQuantity < 2) || (cardsQuantity >7));
 
     const qtdCards = [];
     qtdCards[0]= "`";
     console.log(qtdCards[0]);
   
-    for(i = 0; i < cardsQuantity; i++){
-        qtdCards[0] +=`<div class="card">
+    for(i = 0; i < (2*cardsQuantity); i++)
+    {
+        qtdCards[0] +=`<div class="card card${i+1}">
     <img class="logoImage" src="assets/front.png" alt="card"/>
 </div>
-`  
-        qtdCards[0] +=`<div class="card">
-<img class="logoImage" src="assets/front.png" alt="card"/>
-</div>
-`  
-    }
-  
+` 
+    } 
     qtdCards[0] +="`";
-  
+
     console.log(qtdCards[0]);
 
     let loadCards = document.querySelector(".card-wrapper");
